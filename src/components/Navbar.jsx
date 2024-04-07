4a25ca01e258fb7acdd370fa198c1ed85267fac7
Anonymous <103519498+twilight2368@users.noreply.github.com>
2024-04-07 21:56:27 +0700

    now

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import Logo from "../assets/images/books.png";
+import { Button, Input } from "@material-tailwind/react";
+import { CiSearch } from "react-icons/ci";
+
+export default function MyNavbar() {
+  return (
+    <div className=" w-full">
+      <div className="h-24 w-full shadow-md  grid grid-cols-5 items-center justify-center align-baseline">
+        <div className=" pl-28 h-full flex gap-1 items-center  select-none col-span-2">
+          <img src={Logo} alt="" className=" block h-20" />
+          <div className="madimi-one-regular text-3xl text-blue-300">
+            Book Exchange
+          </div>
+        </div>
+        <div></div>
+        <div>
+          <div className="relative flex w-full max-w-[24rem]">
+            <Input
+              type="text"
+              label="Search"
+              color="blue"
+              className="pr-20 "
+              containerProps={{
+                className: "min-w-0",
+              }}
+            />
+            <Button
+              size="sm"
+              color={true ? "gray" : "blue-gray"}
+              disabled={false}
+              className="!absolute right-1 top-[2.8px] rounded flex items-center justify-center bg-blue-300"
+            >
+              <CiSearch className=" text-lg text-white " />
+            </Button>
+          </div>
+        </div>
+        <div className=" flex gap-3 justify-center items-center">
+          <Button
+            size="md "
+            variant="outlined"
+            className=" border-blue-300 text-blue-500 montserrat-font"
+          >
+            Register
+          </Button>
+          <Button
+            size="md"
+            varient="filled"
+            className=" bg-blue-500 montserrat-font"
+          >
+            Login
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MyNavbar from "../components/Navbar";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <div className="navbar">
+        <MyNavbar />
+      </div>
+      <div></div>
+    </div>
+  );
+}
