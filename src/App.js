3329228b0b343e9a3d6d334afcaed0e36ea03a97
Alonso1010001 <83605288+Alonso1010001@users.noreply.github.com>
2024-04-17 17:56:56 +0700

    Book detail

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import Home from "./pages/Home";
-
+import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import BookDetail from "./components/BookDetail"
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Home/>}/>
+        
+        <Route path = "/book/:id" element = {<BookDetail />}/>
+      </Routes>
+      </BrowserRouter>
+      <Footer/>
     </div>
   );
 }
