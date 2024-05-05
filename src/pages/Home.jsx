fd23e867ca3f2d300742b3d20b51c4ba42d58081
Anonymous <103519498+twilight2368@users.noreply.github.com>
2024-05-05 23:10:06 +0700

    now-editing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,5 @@ import { MySidebar } from "../components/SideBar";
 import WrapBar from "../components/WrapBar";
 
 export default function Home() {
-<<<<<<< HEAD
-  return (
-    <WrapBar>
-      hello
-    </WrapBar>
-  );
-=======
   return <></>;
->>>>>>> a9419aa (now)
 }
