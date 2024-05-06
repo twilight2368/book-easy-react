59cc65529aea1df01fa6d19157b5f7db8d657180
Anonymous <103519498+twilight2368@users.noreply.github.com>
2024-05-06 10:43:42 +0700

    now

--- src/components/books/BookDisplaySmall.jsx
+++ src/components/books/BookDisplaySmall.jsx
@@ -4,14 +4,10 @@ import "./bookdisplay.css";
 import { IconButton } from "@material-tailwind/react";
 import { StarIcon } from "@heroicons/react/24/outline";
 
-export default function BookDisplay() {
+export default function BookDisplaySmall() {
   return (
     <div className=" relative w-11/12 h-full duration-300 hover:scale-105 book-display overflow-hidden rounded-md">
       <img src={BookCover} alt="" className=" object-fill h-full w-full" />
-      <div className=" absolute bottom-0 bg-black/75 w-full text-white text-center whitespace-nowrap text-nowrap book-name-display">
-        <div className=" font-black text-lg nunito-font mt-0.5">Dune</div>
-        <div className=" nunito-font mb-0.5">Frank Herbert</div>
-      </div>
       <div className=" absolute top-2 right-2 star-icon-display">
         <IconButton className="bg-black/75">
           <StarIcon className="h-5 w-5 fill-white star-icon" />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,39 @@
 import React from "react";
-import MyNavbar from "../components/Navbar";
-import { MySidebar } from "../components/SideBar";
-import WrapBar from "../components/WrapBar";
+import BookListHorizontal from "../components/book-list/book-list-horizontal";
+import EventMain from "../components/event-main/event-main";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div className="  pl-10 pr-10 pt-5">
+        <div className="w-full pl-10 mb-5 min-h-full">
+          <EventMain />
+        </div>
+        <div className="w-full pl-5 mb-5 min-h-full">
+          <h2 className="pl-5 font-black text-2xl">Books</h2>
+          <BookListHorizontal />
+        </div>
+        <div className="w-full pl-5 mb-5 min-h-full">
+          <h2 className="pl-5 font-black text-2xl">Books</h2>
+          <BookListHorizontal />
+        </div>
+        <div className="w-full pl-5 mb-5 min-h-full">
+          <h2 className="pl-5 font-black text-2xl">Books</h2>
+          <BookListHorizontal />
+        </div>
+        <div className="w-full pl-5 mb-5 min-h-full">
+          <h2 className="pl-5 font-black text-2xl">Books</h2>
+          <BookListHorizontal />
+        </div>
+        <div className="w-full pl-5 mb-5 min-h-full">
+          <h2 className="pl-5 font-black text-2xl">Books</h2>
+          <BookListHorizontal />
+        </div>
+        <div className="w-full pl-5 mb-5 min-h-full">
+          <h2 className="pl-5 font-black text-2xl">Books</h2>
+          <BookListHorizontal />
+        </div>
+      </div>
+    </>
+  );
 }
