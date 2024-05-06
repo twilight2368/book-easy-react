cade3fd0062c9caadb6cb7fd146e4bdc0904e28b
Anonymous <103519498+twilight2368@users.noreply.github.com>
2024-05-06 22:55:02 +0700

    now

---
+++ src/components/book-list/book-list-horizontal.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function BookListHorizontal() {
+  return (
+    <div></div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,13 @@ import { MySidebar } from "../components/SideBar";
 import WrapBar from "../components/WrapBar";
 
 export default function Home() {
+<<<<<<< HEAD
   return (
     <WrapBar>
       hello
     </WrapBar>
   );
+=======
+  return <></>;
+>>>>>>> a9419aa (now)
 }
